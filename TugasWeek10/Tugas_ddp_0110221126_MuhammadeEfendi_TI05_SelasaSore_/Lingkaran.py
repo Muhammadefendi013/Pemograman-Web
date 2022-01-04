8e28c39299b84d3fcb3035d3701cf1371be0d7e4
Muhammadefendi013 <92834785+Muhammadefendi013@users.noreply.github.com>
2022-01-04 16:59:18 +0700

    Create Lingkaran.py

---
+++ TugasWeek10/Tugas_ddp_0110221126_MuhammadeEfendi_TI05_SelasaSore_/Lingkaran.py
@@ -0,0 +1,10 @@
+import math
+
+print ("Keliling dan Luas Lingkaran")
+r = float(input("Masukan Nilai Jari-Jari : "))
+
+luas = math.pi*(r*r)
+keliling = 2*math.pi*r
+
+print ("Luas Lingkaran \t\t:", luas)
+print ("Keliling Lingkaran \t:", keliling)
